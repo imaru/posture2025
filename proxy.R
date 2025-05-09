4a2c7d83cc7e9cb0c70d47df463248bc7837b3eb
Imaruoka, Toshihide <imaru@neptune.kanazawa-it.ac.jp>
2025-05-09 10:06:17 +0900

    del pass

--- proxy.R
+++ proxy.R
@@ -1,4 +1,2 @@
 Sys.setenv("https_proxy"="http://wwwproxy.kanazawa-it.ac.jp:8080/")
 Sys.setenv("http_proxy"="http://wwwproxy.kanazawa-it.ac.jp:8080/")
-
-# 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 github token
